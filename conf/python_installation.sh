555f3e813738893bfbf5ce22853afca6ad37103b
vrbad <david.vrba@emplifi.io>
2023-03-23 06:44:41 +0100

    upgrade to Spark 3.3.2

--- conf/python_installation.sh
+++ conf/python_installation.sh
@@ -17,10 +17,10 @@ pip3 install Pillow
 pip3 install tensorflow
 pip3 install jupyter
 
-wget /home/$student https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz
-tar xvf spark-3.3.0-bin-hadoop3.tgz
+wget /home/$student https://archive.apache.org/dist/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+tar xvf spark-3.3.2-bin-hadoop3.tgz
 
-SPARK_HOME=/home/$student/spark-3.3.0-bin-hadoop3
+SPARK_HOME=/home/$student/spark-3.3.2-bin-hadoop3
 
 echo 'export SPARK_HOME='$SPARK_HOME >> ~/.bashrc
 echo 'export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH' >> ~/.bashrc
